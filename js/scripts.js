f9363bb601579edf89828399feb1c7d04335ea81
neha doodi <nehadoodipoonia@gmail.com>
2020-01-23 10:56:59 -0800

    delet var vowels, use vowels in 'if' condition

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@
 $(document).ready(function() {
     $("#pigLatin").submit(function(event) {
         event.preventDefault();
-        var vowel = "a";
+        // var vowel = ["a","e","i","o","u"];
         var word1 = "way";
         var text = $("input#text").val();
+        console.log(1)
         
-        if (text.charAt(0) === vowel) {
-            
+        if (text.charAt(0) === "a" || "e" || "i" || "o" || "u") {
+            console.log(2)
             text= text.concat(word1);
+            console.log(3)
             alert(text);
         }
         
