e1f6ffc7c8423fccab0bbaab09e06fded2c8064d
neha doodi <nehadoodipoonia@gmail.com>
2020-01-23 23:06:13 -0800

    add if condition for situation when y is a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
 
     $("#pigLatin").submit(function(event) {
         event.preventDefault();
-        // var vowel = ["a","e","i","o","u"];
-        // var word1 = "way";
         var text =  $("input#text").val().toString();
         var array = text.split(" ");
         
@@ -25,9 +23,14 @@ $(document).ready(function() {
         function pigLatin(text) {
             if (vowelCheck(text.charAt(0))===true) {
                 text = text.concat("way");      
-            } else {
+            } else if (text.length>=2 && text.charAt(0) ==="q" && text.charAt(1)==="u") {
+                var text1 = text.substring(0,2);
+                var text2 = text.substring(2,text.length);
+                text = text2.concat(text1);
+            }
+             else {
                 for(var i=1; i<text.length; i++) {
-                    if (vowelCheck(text.charAt(i))===true) {
+                    if (vowelCheck(text.charAt(i))===true || text.charAt(i)=== "y") {
                         var text1 = text.substring(0,i);
                         var text2 = text.substring(i,text.length);
                         var newtext = text2.concat(text1).concat("ay");
